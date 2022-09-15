34037981e688b8db1f4465ba35f1a8951c670737
waghmarepragati21 <112954487+waghmarepragati21@users.noreply.github.com>
2022-09-15 18:27:43 +0530

    Add files via upload

---
+++ 15-09-22/P2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class P2 {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.println(" enter the first limit");
+int n1 = input.nextInt();
+System.out.println(" enter the second limit ");
+int n2 = input.nextInt();
+int f=0;
+for (int i=n1;i<=n2;i++)
+{
+f=0;
+for(int j=1;j<=i;j++)
+{
+if(i%j==0)
+f++;
+}
+if(f==2)
+System.out.print(i +"\t");
+} }
+}
---
+++ 15-09-22/Salary.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Salary {
+public static void main(String[] args) {
+int emp1 = 1100;
+int emp2 = 500;
+int avg = 30000;
+System.out.println("Third Employee Earn" + (emp1+emp2-avg));
+}
+}
