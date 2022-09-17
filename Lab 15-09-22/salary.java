eebc1edb50b20b84aa7a7c7cc3f8d5f689b1fde5
waghmarepragati21 <112954487+waghmarepragati21@users.noreply.github.com>
2022-09-17 23:47:07 +0530

    Add files via upload

---
+++ Lab 15-09-22/salary.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Salary {
+public static void main(String[] args) {
+int employee1 = 11000;
+int employee2 = 5000;
+int avg = 30000;
+System.out.println("Third Employee Earn" + (emp1+emp2-avg));
+}
+}
