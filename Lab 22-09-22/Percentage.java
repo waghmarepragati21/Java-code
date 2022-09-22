70f6f58ae9a44ecc8ce30e9702130e53bb23d202
waghmarepragati21 <112954487+waghmarepragati21@users.noreply.github.com>
2022-09-22 07:12:52 -0700

    Add files via upload

---
+++ Lab 22-09-22/Percentage.java
@@ -0,0 +1,13 @@
+class Percentage 
+{
+	public static void main(String[] args) 
+	{
+		long popul2 = 175000 ;
+			long popul1 =262500 ;
+			long diff = popul1-popul2 ;
+		long precent = (diff*100)/popul2 ;
+	
+
+System.out.println("Average growth in population is " +  precent/10);
+}
+}
