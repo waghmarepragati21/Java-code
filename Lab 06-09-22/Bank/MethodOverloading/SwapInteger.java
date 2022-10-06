f5773441cac7db5bd6aa466e12d41ada74b5de40
waghmarepragati21 <112954487+waghmarepragati21@users.noreply.github.com>
2022-10-06 15:29:11 -0700

    Add files via upload

---
+++ Lab 06-09-22/Bank/MethodOverloading/SwapInteger.java
@@ -0,0 +1,19 @@
+package lab_06oct;
+
+public class SwapInteger {
+	
+	  public static void main(String[] args) {
+		  
+		  display(10, 90);
+	      display(10.0, 90.0);
+
+	    }
+	 public static void display(int a, int b){
+	        System.out.println("Integer: " + a + " and " + b);
+	    }
+	
+	   
+	    public static void display(double a, double b){
+	        System.out.println("Double " + a + " and " + b);
+	    }
+}
